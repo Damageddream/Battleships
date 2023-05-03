eeb07cf364d6154ccf44d0088953c4a96475bba8
Damageddream <marcan.grab@gmail.com>
2023-05-03 18:36:04 +0200

    small fixes

--- src/game.js
+++ src/game.js
@@ -10,8 +10,8 @@ export const game = () => {
         // Player1.board.placeShip("submarine1", ['a1'])
         // Player1.board.placeShip("submarine2", ['c1'])
         // Player1.board.placeShip("destroyer1", ['e1','e2'])
-        Player1.board.placeShip("destroyer2", ['g2','h2'])
-        // Player1.board.placeShip("cruiser", ['a4','b4', 'c4'])
+        // Player1.board.placeShip("destroyer2", ['g2','h2'])
+        Player1.board.placeShip("cruiser", ['a4','b4', 'c4'])
         // Player1.board.placeShip("battleship", ['e4','e5', 'e4', 'e7'])
         // Player1.board.placeShip("aircraftCarrier", ['h4','h5', 'h4', 'h7', 'h8'])
     }
@@ -20,8 +20,8 @@ export const game = () => {
         // Computer.board.placeShip("submarine1", ['a1'])
         // Computer.board.placeShip("submarine2", ['c1'])
         // Computer.board.placeShip("destroyer1", ['e1','e2'])
-        Computer.board.placeShip("destroyer2", ['g2','h2'])
-        // Computer.board.placeShip("cruiser", ['a4','b4', 'c4'])
+        // Computer.board.placeShip("destroyer2", ['g2','h2'])
+        Computer.board.placeShip("cruiser", ['a4','b4', 'c4'])
         // Computer.board.placeShip("battleship", ['e4','e5', 'e4', 'e7'])
         // Computer.board.placeShip("aircraftCarrier", ['h4','h5', 'h4', 'h7', 'h8'])
     }
